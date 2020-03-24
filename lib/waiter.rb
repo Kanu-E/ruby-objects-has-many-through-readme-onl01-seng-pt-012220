de5bb87ce0627256709a53ed91ec13a87e636ee7
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-03-24 22:30:04 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,36 @@
 class Waiter
-
+ 
+  attr_accessor :name, :yrs_experience
+ 
+  @@all = []
+ 
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end
+  
+  def waiters
+   meals.map {|meal| meal.waiter}
+  end
+  def meals
+  Meal.all.select {|meal| meal.waiter == self}
+  end
+  def best_tipper
+   def best_tipper
+   x = self.meals.map {|meal| meal}
+  y = x.sort_by {|z| z.tip}
+  z = y[-1]
+  z.customer
+end
+ 
+ end
+  
 end
